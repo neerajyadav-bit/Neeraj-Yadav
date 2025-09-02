8a9b2ff948bb3188d8ed253830afa5594c201357
unknown <neerajyadav>
2025-09-02 15:47:15 +0530

    msg

--- hello.c
+++ hello.c
@@ -1,1 +1,60 @@
-kkghgghe
+#include<stdio.h>
+int main()
+{
+
+ for(int i=0;i<50;i++){
+
+
+// printf("%d\n",i);
+// if(i%2==0)
+// printf("%d\n",i);
+int num=123;
+int sum=0;
+while (num>0)
+{
+    int lastdigit=num%10;
+    sum=sum+lastdigit;
+    num=num/10;
+
+}
+
+
+// printf("%d\n",sum);
+
+
+
+
+
+}
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
