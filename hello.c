4db2751e05736f447b39903899a609a8e4d81b69
unknown <neerajyadav>
2025-09-03 06:01:07 +0530

    num3

--- hello.c
+++ hello.c
@@ -8,21 +8,22 @@ int main()
 // printf("%d\n",i);
 // if(i%2==0)
 // printf("%d\n",i);
-int num=123;
-int sum=0;
-while (num>0)
-{
-    int lastdigit=num%10;
-    sum=sum+lastdigit;
-    num=num/10;
+// int num=123;
+// int sum=0;
+// while (num>0)
+// {
+//     int lastdigit=num%10;
+//     sum=sum+lastdigit;
+//     num=num/10;
 
-}
-
-
-// printf("%d\n",sum);
+// }
 
 
+//  printf("%d\n",sum);
 
+
+if( i%2==1)
+printf("%d\n",i);
 
 
 }
