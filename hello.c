58a39c4b47bccb0ab06589dbe7c32dd369511432
unknown <neerajyadav>
2025-09-03 13:59:59 +0530

    loop

--- hello.c
+++ hello.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
-int main()
-{
+int main(){
 
- for(int i=0;i<50;i++){
+
+//  for(int i=0;i<50;i++){
 
 
 // printf("%d\n",i);
@@ -22,16 +22,57 @@ int main()
 //  printf("%d\n",sum);
 
 
-if( i%2==1)
-printf("%d\n",i);
+// if( i%2==1)
+// printf("%d\n",i);
 
+
+// int  num=121;
+// int original=num;
+// int res=0;
+// while (num!=0)
+// {
+//     int lastDigit=num%10;
+//     res=  res*10+ lastDigit;
+//     num=num/10;
+// }
+// if(original==res){
+//     printf("p");
+
+// }else{
+//     printf("not");
+// }
+
+
+// for(int i=0;i<50;i++){
+//     if("i%2!==0;i%2!==1");
+//     printf("%d/n",&i);
+
+// }
+// }
+// int i;
+// int num;
+
+// if(num<=1){
+//     printf("ye nahi hai prime no");
+// }
+// for(int i=2;i*i<=num;i++)
+// {
+//     if (num % i==0){
+//         printf("ye nahi hai prime number");
+//     }
+
+// }
+// printf("prime hai")
+int i,n,fact=1;
+printf("number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+    fact=fact*i;
+}
+printf("Factorial=%d",fact);
 
 }
-
-
-
-
-
 
     
 
@@ -58,4 +99,3 @@ printf("%d\n",i);
 
 
 
-}
