c2dc5ae7bc76bf42943e3851a44b973c7baa9bcc
unknown <neerajyadav>
2025-09-10 12:12:36 +0530

    loop2

--- hello.c
+++ hello.c
@@ -1,101 +1,206 @@
-#include<stdio.h>
-int main(){
-
-
-//  for(int i=0;i<50;i++){
-
-
-// printf("%d\n",i);
-// if(i%2==0)
-// printf("%d\n",i);
-// int num=123;
-// int sum=0;
-// while (num>0)
-// {
-//     int lastdigit=num%10;
-//     sum=sum+lastdigit;
-//     num=num/10;
-
-// }
-
-
-//  printf("%d\n",sum);
-
-
-// if( i%2==1)
-// printf("%d\n",i);
-
-
-// int  num=121;
-// int original=num;
-// int res=0;
-// while (num!=0)
-// {
-//     int lastDigit=num%10;
-//     res=  res*10+ lastDigit;
-//     num=num/10;
-// }
-// if(original==res){
-//     printf("p");
-
-// }else{
-//     printf("not");
-// }
-
-
-// for(int i=0;i<50;i++){
-//     if("i%2!==0;i%2!==1");
-//     printf("%d/n",&i);
-
-// }
-// }
-// int i;
-// int num;
-
-// if(num<=1){
-//     printf("ye nahi hai prime no");
-// }
-// for(int i=2;i*i<=num;i++)
-// {
-//     if (num % i==0){
-//         printf("ye nahi hai prime number");
-//     }
-
-// }
-// printf("prime hai")
-int i,n,fact=1;
-printf("number");
-scanf("%d",&n);
-for(i=1;i<=n;i++)
+#include <stdio.h>
+int main()
 {
-    fact=fact*i;
+
+    //  for(int i=0;i<50;i++){
+
+    // printf("%d\n",i);
+    // if(i%2==0)
+    // printf("%d\n",i);
+    // int num=123;
+    // int sum=0;
+    // while (num>0)
+    // {
+    //     int lastdigit=num%10;
+    //     sum=sum+lastdigit;
+    //     num=num/10;
+
+    // }
+
+    //  printf("%d\n",sum);
+
+    // if( i%2==1)
+    // printf("%d\n",i);
+
+    // int  num=121;
+    // int original=num;
+    // int res=0;
+    // while (num!=0)
+    // {
+    //     int lastDigit=num%10;
+    //     res=  res*10+ lastDigit;
+    //     num=num/10;
+    // }
+    // if(original==res){
+    //     printf("p");
+
+    // }else{
+    //     printf("not");
+    // }
+
+    // for(int i=0;i<50;i++){
+    //     if("i%2!==0;i%2!==1");
+    //     printf("%d/n",&i);
+
+    // }
+    // }
+    // int i;
+    // int num;
+    // int num =100;
+    // if(num<=1){
+    //     printf("%d",num);
+    // }
+    // for(int i=2;i*i<=num;i++)
+    // {
+    //     if (num % i==0){
+    //         printf("prime no nahi hai");
+    //     }
+
+    // }
+    // printf("prime no hai");
+
+    // int i,n,fact=1;
+    // printf("number");
+    // scanf("%d",&n);
+    // for(i=1;i<=n;i++)
+    // {
+    //     fact=fact*i;
+    // }
+    // printf("Factorial=%d",fact);
+
+    // int n=9;
+
+    // int t1=0;
+    // int t2=1;
+    // int next;
+    // for( int i= 1; i<=n;i++){
+    // printf("%d\n",t1);
+    // next=t1+t2;
+    // t1=t2;
+    // t2=next;
+
+    // for(int i=0; i<=10;i++)
+    // {
+    //     if(i%2==0)
+    //     {
+    //         continue;
+    //         // break;
+    //     }
+    //     printf("%d",i);
+    // }
+
+    // ONE LOOP RUNNIG::
+
+    // for( int i=0;i<3;i++){
+    //     printf("\n*");
+    // }
+    // TWO LOOP RUNNING::
+    // for( int i=0;i<5;i++)
+    // {
+    //     for(int j=0;j<3;j++)
+    //     {
+    //         printf("*");
+
+    //     }
+    //     printf()
+    // }
+
+    // for( int i=0;i<5;i++)
+    // {
+    //     for( int j=0; j<3;j++)
+    // {
+    //     printf("*");
+
+    // }
+    // printf("\n");
+
+    // }
+    // }
+
+    // for( int i=0;i<3;i++)
+    // {
+    //     for( int j=0; j<=i;j++)
+    // {
+    //     printf("*");
+
+    // }
+    // printf("\n");
+
+    // }
+    // }
+
+    // for( int i=0;i<3;i++)
+    // {
+    //     for( int j=3; j>i;j--)
+    // {
+    //     printf("*");
+
+    // }
+    // printf("\n");
+
+    // }
+    // }
+
+    // for( int i=0;i<3;i++)
+    // {
+    //     for( int j=0; j<=i;j++)
+    // {
+    //     printf("_");
+
+    // }
+    // printf("\n");
+
+    // }}
+
+    // for( int i=0;i<3;i++)
+    // {
+    //     for( int j=0; j<=i;j++)
+    // {
+    //     printf("%d",j);
+
+    // }
+    // printf("\n");
+
+
+
+    //  for( int i=0;i<3;i++)
+    // {
+    //      for( int j=0; j<=i;j++)
+    //       {
+    //      printf(" ");
+    //     }
+
+    //  for ( int k=3;k>i;k--)
+    // {
+    //     printf("*");
+    // }
+    //  for(int m=1;m<=i;m++)
+    //  {
+    //      printf("*");
+    //   }
+    //   }
+    //   printf("\n");
+    //  }
+     
+
+
+
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            printf(" ");
+        }
+            for (int k = 3; k > i; k--)
+            {
+                printf("*");
+            }
+            for (int m = 1; m >= i; m--)
+            {
+                printf("*");
+            }
+        
+        printf("\n");
+    }
 }
-printf("Factorial=%d",fact);
-
-}
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
