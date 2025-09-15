c245b8aa80c7d0ec46b7eccc692e1c6e8adf905c
unknown <neerajyadav>
2025-09-15 15:51:39 +0530

    array1

---
+++ array.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+int main()
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+    // for( int i=0;i<5;i++){
+    //     printf("%d\n",arr[i]);
+    // }
+
+    // printf("%d\n",arr[3]);
+
+    // if(arr[3]>arr[2])
+    // {
+    //     printf("%d\n",arr[3]);
+    // }
+    // else{
+    //     printf("%d\n",arr[2]);
+    // }
+
+    // for(int i=4;i>=0;i--)
+    // {
+    //     printf("%d\n",arr[i]);
+    // }
+
+    // int sum=0;
+    // for ( int i=0;i<5;i++)
+    // sum=sum+arr[i];
+    // printf("%d\n",sum);
+
+    // int sum;
+    //     for( int i=0;i<3;i++)
+    //     sum = sum+arr[i];
+    //     printf("%d\n",sum);
+
+    // int max =arr[0];
+    // for( int i=0;i<5;i++)
+    // {
+    //     if (arr[i]>max)
+    //     {
+    //         max=arr[i];
+    //         printf("%d\n",max);
+    //     }
+    // }
+
+    // int min =arr[0];
+    // for( int i=4;i>0;i--)
+    // {
+    //     if (arr[i]<min)
+    //     {
+    //         min=arr[i];
+    //         printf("%d\n",min);
+    //     }
+    // }
+
+    // int i;
+    // for(i=0;i<5;i++){
+    // if(arr[i]==5)
+    // {
+    //     printf("%d\n",arr[i]);
+    // }
+
+    // }
+
+    // int i;
+    // for( int i=0;i<5;i++)
+    // {
+    //     if(arr[i]%2==0)
+    //     {
+    //         printf("even no");
+    //     }
+    //     else {
+    //         printf("%d" ,arr[i]);
+    //     }
+
+
+    // int target = 5;
+
+    // for (int i = 0; i < 5; i++)
+
+    // // {
+    // if(arr[i]==target)
+    // {
+    //     // printf("true");
+    //     printf("%d",arr[i]);
+    // }
+    // }
+
+    // {
+    //     if (arr[i] ==)
+    //     {
+    //         printf("%d\n", arr[i]);
+//     //     }
+//     }
+// }
--- lect10.c
+++ lect10.c
@@ -1,27 +1,129 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int n;
-    printf("Enter the  nnumber of n:");
-    scanf("%d",&n);
+    // int n=5;
+    // printf("Enter the  nnumber of n:");
+    // scanf("%d",&n);
 
+    // for( int i=1;i<n;i++)
+    // {
+    //     for( int j=1;j<=i;j++)
+    //  {
+    //     printf("*");
+    //  }
+    //  for( int j=1;j<=2*(n-i);j++)
+    //  {
+    //     printf(" ");
+    //  }
+    //  for( int j=1;j<=i;j++)
+    //  {
+    //     printf("*");
+    //  }
 
-for( int i=1;i<n;i++)
-{
-    for( int j=1;j<=i;j++)
- {
-    printf("*");
- }   
- for( int j=1;j<=2*(n-i);j++)
- {
-    printf(" ");
- }
- for( int j=1;j<=i;j++)
- {
-    printf("*");
- }
- printf("\n");
-}
+    //   printf("\n");
+    // }
 
+    //  }
 
+    // for( int i=1;i<n;i++)
+    // {
+    //     for( int j=n;j>=i;j--)
+    //  {
+    //     printf("*");
+    //  }
+    //  for( int j=n;j<=2*(n-i);j--)
+    //  {
+    //     printf(" ");
+    //  }
+    //  for( int j=n;j>=i;j--)
+    //  {
+    //     printf("*");
+    //  }
+    //   printf("\n");
+    // }
+
+    //  }
+
+    // for( int i=1;i<n;i++)
+    // {
+    //     for( int j=1;j<=i;j++)
+    //  {
+    //     printf("*");
+    //  }
+    //  for( int j=1;j<=2*(n-i);j++)
+    //  {
+    //     printf(" ");
+    //  }
+    //  for( int j=1;j<=i;j++)
+    //  {
+    //     printf("*");
+    //  }
+    //  for( int k=3;k>i;k--)
+    //  {
+    //     printf("*");
+    //  }
+
+    //   printf("\n");
+    // }
+
+    // }
+    // for(int i=0;i<4;i++){
+    //         for(int j=3;j>i;j--){
+    //             printf("*");
+    //         }
+    //         for(int k=0;k<=i;k++){
+    //             printf(" ");
+    //         }
+    //          for(int k=0;k<=i;k++){
+    //             printf(" ");
+    //         }
+    //          for(int j=3;j>i;j--){
+    //             printf("*");
+    //         }
+    //         printf("\n");
+
+    //     }    }
+
+    // for( int i=1;i<=n;i++)
+    // {
+    //     for( int j=1;j<=n;j++)
+    //     {
+    //         if (i=1 || i==n||j==1||j==n)
+    //         {
+    //             printf("*");
+    //         }
+    //         else{
+    //             printf(" ");
+    //         }
+    //         printf("\n");
+    //     }
+    // }
+    //  }
+
+    // int a,b,c;
+    // printf("enter a number");
+    // scanf("%d %d",&a,&b);
+    // c=a+b;
+    // printf(" %d",c);
+
+    int num, i, isprime;
+    printf("Prime number between 1 to 100 are:");
+    for (num = 2; num <= 100; num++)
+    {
+        isprime = 1;
+        for (i = 2; i <= num % 2; i++)
+        {
+            if (num % i == 0)
+            {
+                isprime = 0;
+                break;
+            }
+        }
+        if (isprime == 1)
+        {
+            printf("%d", num);
+        }
+    }
+    printf("\n");
+    return 0;
 }
