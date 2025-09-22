629698d7db224c8e087c17c12b79f0334981beae
unknown <neerajyadav>
2025-09-22 15:59:53 +0530

    pointer1

---
+++ pointer.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+// int main()
+
+// int a=303;
+// int *x= &a;
+
+// printf("%d",*x);
+
+//     int sum(int *x, int *y)
+// {
+//     *x = 50;
+//     *y = 50;
+//     int c = (*x) + (*y);
+//     return c;
+// }
+// int main()
+// {
+//     int x - 10;
+//     int y = 20;
+//     int data = sum(&x, &y);
+//     printf("%d", data);
+//     printf("%d", x + y);
+// }
+// {
+//     int a=920;
+
+//     int  *x=&a;
+//     printf("%d",*x);
+
+// }
+
+// {
+//     int arr[5]={ 1,2,3,4,5};
+//     int *p=arr;
+//     printf("%d",*p);
+//     printf("%d",*(p+1));
+//     printf("%d",*(p+2));
+
+// }
+
+// {
+//     int arr[5] = {1, 2, 3, 4, 5};
+//     int *p = arr;
+//     for (int i= 0; i < 5; i++)
+//     {
+//         printf("%d", *p);
+//     }
+// // }
+
+// int swap(int *x, int *y)
+// {
+//     int temp = *x;
+//     *x = *y;
+//     *y = temp;
+// }
+// int main()
+// {
+//     int x = 10;
+//     int y = 20;
+
+//     printf("%d", swap);
+// // }
+
+//  int main(){
+// int a=5;
+// int b=6;
+// printf("%d %d",a,b);
+// a=a+b;
+// b=a-b;
+// a=a-b;
+// printf("%d %d",a,b);
+
+// }
+
+int main(){ 
+    int arr1[4]={1,2,3,4};
+    int arr2[4]={ 1,2,3,4};
+    int *p1=arr1;
+    int *p2=arr2;
+    int flag=1;
+    for( int i=0;i<5;i++)
+    {
+        if(*(p1+i)! = *(p2+i)){
+            flag
+        }
+    }
+
+
+    
+
+
+
+}
