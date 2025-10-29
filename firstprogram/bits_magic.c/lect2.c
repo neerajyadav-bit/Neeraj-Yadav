76ba91219f88a530765ae9043552bf66f12d3264
unknown <neerajyadav>
2025-10-29 14:57:47 +0530

    lect29

---
+++ firstprogram/bits_magic.c/lect2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+
+
+// {
+
+//     int arr[5]={ 4,5,3,4,5};
+//     int xor=0;
+//     for( int i=0;i<5;i++)
+//     {
+//         xor=xor^arr[i];
+
+//     }
+//     printf("%d",xor);
+
+// }
+
+{
+    int arr[9]={ 2,4,3,2,3,4,5,6,5};
+
+    int xor=0;
+    for( int i=0;i<9;i++)
+    {
+        xor=xor^arr[i];
+
+    }
+    printf("%d",xor);
+}
